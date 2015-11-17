88c2c948cdb788f2eb03c171385db83c8681caed
Olle Lind <mail@ollelind.se>
2015-11-17 22:19:33 +0100

    Initial commit - Project created

---
+++ Habits/Habits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Habits
+//
+//  Created by Olof Lind on 2015-11-17.
+//  Copyright © 2015 Olof Lind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
